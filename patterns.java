b8c673a36a59f37acee7668abb46e2bfc0596895
Mbviswanadh <madhavareddybalajiviswanadh@gmail.com>
2024-01-13 19:58:55 +0530

    double pattern

--- patterns.java
+++ patterns.java
@@ -86,6 +86,36 @@ public class patterns {
             System.out.println();
         }
     }
+
+    static void doublemiddle(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+                for(int i=0;i<n;i++){
+            for(int j=0; j<i;j++){
+                System.out.print(" ");
+            }
+
+            for(int j=0;j<2*n-(2*i+1) ;j++){
+                System.out.print("*");
+            }
+
+            for(int j=0; j<i;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int n = sc.nextInt();
@@ -104,6 +134,8 @@ public class patterns {
         middle(n);
         System.out.println("---\n");
         inversemiddle(n);
+        System.out.println("---\n");
+        doublemiddle(n);
 
 
 
