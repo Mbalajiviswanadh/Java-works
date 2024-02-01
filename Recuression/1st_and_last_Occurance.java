1dd3826a53af418fac4f8ec5c5b63deb7e373322
Mbviswanadh <madhavareddybalajiviswanadh@gmail.com>
2024-02-01 21:40:39 +0530

    1st and last Occurance

---
+++ Recuression/1st_and_last_Occurance.java
@@ -0,0 +1,57 @@
+package Recuression;
+import java.util.*;
+
+ class First_and_Last_Occurance {
+
+    public static int  first =-1;
+    public static int  last=-1;
+
+    //fucntion for finding occurance
+    public static void Finding_Occurance(String str, int index, char element_tofind){
+
+        //base case
+
+        if(index==str.length()){
+            System.out.print("Starting place :"+first+"\n");
+            System.out.print("Last place :"+last);
+            return;
+        }
+
+
+
+        //getting the charater from the string 
+        char current_charater = str.charAt(index);
+
+        if(current_charater == element_tofind ){
+
+            if(first==-1){
+                first =index;
+            }
+            else{
+                last= index;
+            }
+        }
+        //recuression
+        Finding_Occurance(str,index+1,element_tofind);
+    }
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Type letters: ");
+
+        String str= sc.nextLine();
+
+        System.out.print("Enter One Character to find 1st and Last place{indices} :");
+
+        String charInput=sc.next();
+
+        // char element_tofind = sc.charAt;
+        char element_tofind = charInput.charAt(0);
+
+
+        //calling function
+        Finding_Occurance(str, 0, element_tofind);
+
+
+    }
+}
