2502d6b3cfcb0ca65b84344758df4ba35cb40b48
Mbviswanadh <madhavareddybalajiviswanadh@gmail.com>
2024-02-02 23:05:43 +0530

    Two Sum

--- LeetCode/Two_Sum_leetcode.java
+++ LeetCode/Two_Sum_leetcode.java
@@ -1,44 +1,48 @@
-// package LeetCode;
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// class Two_Sum {
-//     public static int[] twoSum(int[] nums, int target) {
+class Two_Sum {
+    public static int[] twoSum(int[] nums, int target) {
         
-//         for(int i=0;i<nums.length;i++){
-//             for(int j =i+1;j<nums.length;j++){
-//                 if(nums[i]+nums[j]==target){
+        for(int i=0;i<nums.length;i++){
+            for(int j =i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+
+                    int[] result = {i,j};
                     
-//                     return new int[] {i,j};                
-//                 }
-
-           
-
-//             }
+                    return result;                
+                }
+            }
             
-//         }
-//         return nums;       
+        }
+        return nums;       
 
-//     }
+    }
 
 
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-//         System.out.println("Enetr the size of array:");
-//         int n = sc.nextInt();
-//         int nums[] = new int[n];
-//         System.out.println("Enter the num values:");
-//         for(int i =0;i<n;i++){
-//             nums[i]=sc.nextInt();
-//         }
-//         System.out.println("Enter the targer value:");
-//         int target =sc.nextInt();
-        
-        
-//         twoSum(nums, target);
+        System.out.println("Enetr the size of array:");
+        int n = sc.nextInt();
+        int nums[] = new int[n];
+        System.out.println("Enter the num values:");
+        for(int i =0;i<n;i++){
+            nums[i]=sc.nextInt();
+        }
+        System.out.println("Enter the targer value:");
+        int target =sc.nextInt();
+            
 
         
+        int[] result = twoSum(nums, target);
 
-//     }
-// }
+
+        System.out.println("The final Result is : " +result[0] +"," +result[1]);
+        
+        
+
+        
+
+    }
+}
 
