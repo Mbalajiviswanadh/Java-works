45a8f75b03f93b2f62395ae5ea1b62e819155297
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-08 21:44:00 +0530

    max_equilirbium

--- Uni_STS/max_Equilibrium.java
+++ Uni_STS/max_Equilibrium.java
@@ -1,8 +1,29 @@
 package Uni_STS;
 import java.util.*;
 
-class max_equlibrim {
+class max_equilibrium {
+
+    public static int finding_equilibrium(int arr[] , int n){
+
+        
+    }
     
 
     
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array : ");
+
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("ENter the values inside the array :");
+
+        for(int i =0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+    }
 }
