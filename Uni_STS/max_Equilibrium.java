5f2cd0c69cafa447f989003c828ee0ab3951988f
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-08 21:49:19 +0530

    max Equilibrium

--- Uni_STS/max_Equilibrium.java
+++ Uni_STS/max_Equilibrium.java
@@ -5,6 +5,30 @@ class max_equilibrium {
 
     public static int finding_equilibrium(int arr[] , int n){
 
+        int left_sum = 0;
+        int right_sum=0;
+
+
+
+        for(int i=1; i<n;i++){
+
+
+            //below for loops will find the sum from 0 --- (<i) values
+            for(int j =0; j<i;j++){
+                left_sum +=arr[j];
+            }
+
+            //below for loop wll find the sum after the i to the end of array
+
+            for(int j=i=1; j<n;j++){
+                right_sum=arr[j];
+            }
+
+            if(left_sum == right_sum){
+                return i;
+            }
+        }
+        return 0;
         
     }
     
