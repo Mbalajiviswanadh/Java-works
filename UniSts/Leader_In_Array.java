149f197b94ae8c8ddb9824bdaf5af12b3093f876
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-09 09:49:10 +0530

    leaders using O(n)

--- UniSts/Leader_In_Array.java
+++ UniSts/Leader_In_Array.java
@@ -7,7 +7,7 @@ class Leader_In_Array {
 
         for(int i=0;i<size;i++){
             int j;
-			
+
             for( j=i+1;j<size;j++){
 
                 if( arr[i] <=arr[j]){
@@ -20,7 +20,7 @@ class Leader_In_Array {
 			}
         }
     }
-    
+	   
 
     public static void main (String [] args){
         Scanner sc = new Scanner(System.in);
---
+++ UniSts/Leader_O_n.java
@@ -0,0 +1,45 @@
+package UniSts;
+
+import java.util.*;
+class Leader_O_n {
+
+     void Leaderin_array(int arr[] , int size){
+
+        int max_value =0;
+
+        // itrating from end of the array to the start of the arry 
+        for(int i=size-1; i>=0;i--){
+
+            if(max_value < arr[i]){
+                // if the i value is greater than the max_vales then we are storing that value in max_value
+
+                max_value = arr[i];
+                System.out.println("Max values :" +max_value);
+                
+            }
+        }
+
+    }
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the size of an array : ");
+        int n = sc.nextInt();
+
+
+        int arr[]  = new int[n];
+
+        
+        System.out.println("Enter the values : ");
+        
+        for(int i=0; i<n; i++){
+            arr[i] =sc.nextInt();
+        }
+        int size = arr.length;
+
+        Leader_O_n solution = new Leader_O_n();
+        solution.Leaderin_array(arr, size);
+    }
+    
+}
