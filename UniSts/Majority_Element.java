34c4dd790df85c35f1670751c203efd69bab83ee
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-09 10:44:34 +0530

    Majority value in an array

---
+++ UniSts/Majority_Element.java
@@ -0,0 +1,52 @@
+package UniSts;
+
+import java.util.*;
+
+class Majority_Element {
+
+    public static int finding_majority(int arr[] , int n){
+
+        // in the begging we are taking 1st index=0 as the majority values
+        int majorirty_value =arr[0];
+        int count =0;
+
+        for(int i=1;i<n;i++){
+
+            //if the count is 0 then we are taking the i value in majotiyt values
+            if( count ==0){
+                count++;
+                majorirty_value =arr[i];
+            }
+            //  here we are comparing the majority value with the next i values 
+            else if(majorirty_value == arr[i]){
+                // if the majorirty value is equal to next i values , we need to increment the count value
+                count++;
+            }
+            // if not equle we need to decrement the count value 
+            else{
+                count--;
+            }
+        }
+        return majorirty_value;
+    }
+    
+
+    public static void main ( String [] args ){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Size of an array :");
+
+        int n =sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter the values :");
+
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int result = finding_majority(arr, n);
+        System.out.print("The majority value in an array is :" +result);
+
+
+    }
+}
