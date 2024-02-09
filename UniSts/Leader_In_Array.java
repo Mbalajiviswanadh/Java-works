57d91cb61ecfe60db69058536f910c66d615dcda
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-09 09:20:19 +0530

    Leader in Array

--- UniSts/Leader_In_Array.java
+++ UniSts/Leader_In_Array.java
@@ -1,77 +1,45 @@
 package UniSts;
 
-// import java.util.*;
+import java.util.*;
+class Leader_In_Array {
 
-// public class Leader_In_Array {
+    void leaderin_array(int arr[] , int size){
 
-//     void leaderin_array(int arr[] , int size){
+        for(int i=0;i<size;i++){
+            int j;
+			
+            for( j=i+1;j<size;j++){
 
-//         for(int i=0;i<size;i++){
-//             int j;
-//             for( j=i+1;j<size;j++){
-//                 if( arr[i] <=arr[j]){
-//                     break;
-//                 }
-//                 if( j ==size){
-//                     System.out.println(arr[i]+ " ");
-//                 }
-//             }
-//         }
-//     }
+                if( arr[i] <=arr[j]){
+                    break;
+                }
+            }
+			
+			if( j ==size){
+				System.out.print(arr[i]+ " ");
+			}
+        }
+    }
     
 
-//     public static void main (String [] args){
-//         Scanner sc = new Scanner(System.in);
-//         System.out.println("Enter the size of an array :");
-//         int y = sc.nextInt();
-
-
-
-//         Leader_In_Array solution = new Leader_In_Array();
-//         int arr[] = new int[y];
-
-//         System.out.println("Enter values :");
-//         for(int i=0;i<y;i++){
-//             arr[i]= sc.nextInt();
-//         }
-//         int n = arr.length;
-
-
-//         solution.leaderin_array(arr, n);
-//     }
-// }
-
-import java.io.*;
-import java.util.Scanner;
- class LeadersInArray 
-{
-	/*Java Function to print leaders in an array */
-	void printLeaders(int arr[], int size) 
-	{
-		for (int i = 0; i < size; i++) 
-		{
-			int j;
-			for (j = i + 1; j < size; j++) 
-			{
-				if (arr[i] <=arr[j])
-					break;
-			}
-			if (j == size) // the loop didn't break
-				System.out.print(arr[i] + " ");
-		}
-	}
-
-	/* Driver program to test above functions */
-	public static void main(String[] args) 
-	{
+    public static void main (String [] args){
         Scanner sc = new Scanner(System.in);
-        int y = sc.nextInt();
-		LeadersInArray lead = new LeadersInArray();
-		int arr[] = new int[y];
-        for(int i=0;i<y;i++){
-            arr[i]=sc.nextInt();
+        System.out.println("Enter the size of an array :");
+        int n = sc.nextInt();
+
+
+
+        int arr[] = new int[n];
+		
+        System.out.println("Enter values :");
+        for(int i=0;i<n;i++){
+			arr[i]= sc.nextInt();
         }
-		int n = arr.length;
-		lead.printLeaders(arr, n);
-	}
+		
+		
+		Leader_In_Array solution = new Leader_In_Array();
+		System.out.print("Result :");
+        solution.leaderin_array(arr, n);
+    }
 }
+
