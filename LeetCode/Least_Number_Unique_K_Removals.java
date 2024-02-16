6cd901195723079943fc6239684b7bf42400e648
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-16 23:28:36 +0530

    :)

--- LeetCode/Least_Number_Unique_K_Removals.java
+++ LeetCode/Least_Number_Unique_K_Removals.java
@@ -20,6 +20,21 @@ class Least_Number_Unique_K_Removals {
     }    
 
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int n= sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter the values inside :");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the K {no of removels} value :");
+        int k= sc.nextInt();
+
+        // findLeastNumOfUniqueInts(arr, k);
+        System.out.println("The FInal Result :"+ findLeastNumOfUniqueInts(arr, k)    );
     }
 }
