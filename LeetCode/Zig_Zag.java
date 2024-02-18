fdba6e35f8ca71432eb2b45d4d0c1ebde34a8355
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-18 16:54:18 +0530

    :)

--- LeetCode/Zig_Zag.java
+++ LeetCode/Zig_Zag.java
@@ -2,42 +2,42 @@ import java.util.*;
 
 class Zig_zag{
 
-    // public static String conver(String s, int numRows){
-    //     HashMap<Integer,String> hm = new HashMap<>();
+    public static String conver(String s, int numRows){
+        HashMap<Integer,String> hm = new HashMap<>();
 
-    //     int index=0;
-    //     int row=1;
-    //     boolean increase = true;
+        int index=0;
+        int row=1;
+        boolean increase = true;
 
-    //     while(index < s.length()){
+        while(index < s.length()){
 
-    //         // check if there is a string for this currecnt row
+            // check if there is a string for this currecnt row
 
-    //         String curString = hm.getOrDefault(row,"");
-    //         hm.put(row,curString + s.charAt(index++));
+            String curString = hm.getOrDefault(row,"");
+            hm.put(row,curString + s.charAt(index++));
 
-    //         //check if we get up a row or increase 
-    //         if(increase){
-    //             row++;
-    //             //condition for row if it reaches the last row
-    //             if(row == numRows){
-    //                 increase = false;
-    //             }
-    //         }
-    //         else if(!increase){
-    //             row--;
-    //             if(row ==1){
-    //                 increase = true;
-    //             }
-    //         }
-    //     }
+            //check if we get up a row or increase 
+            if(increase){
+                row++;
+                //condition for row if it reaches the last row
+                if(row == numRows){
+                    increase = false;
+                }
+            }
+            else if(!increase){
+                row--;
+                if(row ==1){
+                    increase = true;
+                }
+            }
+        }
 
-    //     String ans ="";
-    //     for(int r : hm.keySet()){
-    //         ans= ans+hm.get(r);
-    //     }
-    //     return ans;
-    // }
+        String ans ="";
+        for(int r : hm.keySet()){
+            ans= ans+hm.get(r);
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the String :");
