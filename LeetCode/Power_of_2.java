2aff57e0761465499fc89017a4fc6d3e7be054ff
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-19 12:19:57 +0530

    ^^

---
+++ LeetCode/Power_of_2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Power_of_2 {
+
+    // public static boolean ispowerof2_Equlas_toNumber(int n){
+
+    //     for(int i=0;i<31;i++){
+    //         int ans = (int)Math.pow(2, i);
+    //         if(ans ==n)
+    //         return true;
+    //     }
+    //     return false;
+    // }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number :");
+        int n =sc.nextInt();
+
+        // System.out.println("Result : "+ispowerof2_Equlas_toNumber(n));
+    }
+}
