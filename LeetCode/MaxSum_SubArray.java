942ef1a9c3d342f9aee8f8e3ebd0aa72e6f9e0dc
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-20 10:07:22 +0530

    Max Sub array Sum

--- LeetCode/MaxSum_SubArray.java
+++ LeetCode/MaxSum_SubArray.java
@@ -2,6 +2,22 @@ import java.util.*;
 
 public class MaxSum_SubArray{
 
+    public static int FindingMaxSum(int nums[],int n){
+
+        int maxSub=nums[0];
+        int curSum=0;
+
+        for(int i=0;i<n;i++){
+            // if the current sum is less than 0 then reset the current sum to 0
+            if(curSum<0){
+                curSum=0;
+            }
+            curSum+=nums[i];
+            maxSub=Math.max(maxSub,curSum);
+        }
+        return maxSub;
+    }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -13,6 +29,8 @@ public class MaxSum_SubArray{
         for(int i=0;i<n;i++){
             nums[i]=sc.nextInt();
         }
+
+        System.out.println("The Max Subarray Sum is : " +FindingMaxSum(nums, n));
 
     }
 }
