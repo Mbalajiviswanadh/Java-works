7d443d03a7601ef3ffc86c51bd707e6c1a1a580f
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-20 10:40:43 +0530

    Missing Num

---
+++ General_Practice/Missing_num.java
@@ -0,0 +1,50 @@
+package General_Practice;
+import java.lang.reflect.Array;
+import java.util.*;
+public class Missing_num {
+
+    public static int FindMissingNum(int nums[], int n){
+
+        Arrays.sort(nums);
+        int arr[] = new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=i+1;
+        }
+
+        int nums_sum=0;
+        int arr_sum=0;
+
+        for(int i=0;i<n;i++){
+            nums_sum+=nums[i];
+            arr_sum+=arr[i];
+        }
+
+        int missing_value=0;
+
+        for(int i=0;i<n;i++){
+
+            if(arr_sum ==nums_sum){
+                return missing_value;
+            }else{
+                 missing_value=arr_sum -nums_sum;
+            }
+            return missing_value;
+        }
+        return missing_value;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size of the Array:");
+        int n=sc.nextInt();
+
+        int nums[] = new int[n];
+        System.out.println("Enter the values inside:");
+        for(int i=0;i<n;i++){
+            nums[i] =sc.nextInt();
+        }
+        // Missing_num sol= new Missing_num();
+        System.out.println("Result : " +FindMissingNum(nums, n));
+        // sol.FindMissingNum(nums,n);
+
+    }
+}
