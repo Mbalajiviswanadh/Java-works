da2bc3b783e338458cee9337add455daeb3f833d
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-20 10:02:32 +0530

    maxsum sub array

---
+++ LeetCode/Max_SubArray.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Max_SubArray{
+    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an array :");
+        int n = sc.nextInt();
+
+        int nums[] = new int[n];
+        System.out.println("Enter the values inside:");
+        for(int i=0;i<n;i++){
+            nums[i]=sc.nextInt();
+        }
+
+    }
+}
