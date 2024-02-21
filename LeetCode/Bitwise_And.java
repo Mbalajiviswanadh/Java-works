21bb956b53077481cc3541805897d36b2e5f984c
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-21 22:46:20 +0530

    ")

--- LeetCode/Bitwise_And.java
+++ LeetCode/Bitwise_And.java
@@ -1,17 +1,28 @@
 import java.util.*;
 public class Bitwise_And {
     public static int bitwise_And(int left , int right){
+
+        /* -----------Recursion----------- 
         // base case 
-        if(left==right)
-            return left;
+        // if(left==right)
+        //     return left;
 
-        int mid = (left+right)/2;
+        // int mid = (left+right)/2;
 
-        int left_Side_AND =bitwise_And(left, mid);
-        int right_Side_AND = bitwise_And(mid+1, right);
+        // int left_Side_AND =bitwise_And(left, mid);
+        // int right_Side_AND = bitwise_And(mid+1, right);
 
-        return left_Side_AND & right_Side_AND;
+        // return left_Side_AND & right_Side_AND;
+        */
+
+        int bitwiseANDResult=left;
+
+        for(int i=left+1;i<=right;i++){
+           return  bitwiseANDResult &=i;
+        }
+        return bitwiseANDResult;
     }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the left and right values:");
