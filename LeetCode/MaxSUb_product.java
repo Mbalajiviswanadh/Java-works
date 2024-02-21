80e489f4a0c029ef51caae362b4e61ef38de7999
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-22 00:32:39 +0530

    :}

--- LeetCode/MaxSUb_product.java
+++ LeetCode/MaxSUb_product.java
@@ -1,5 +1,27 @@
 import java.util.*;
 public class MaxSUb_product {
+
+        public static  int finding_maxProduct(int nums[],int n){
+
+            int ans=nums[0];
+            int leftProduct=1;
+            int rightProduct=1;
+
+            for(int i=0;i<n;i++){
+
+                if(leftProduct==0 && rightProduct==0){
+                    leftProduct=1;
+                    rightProduct=1;
+                }
+                /*
+                 * or
+                 * leftProduct= leftProduct==0?1:leftProduct;
+                 * rightProduct=rightProduct==0?1:rightProduct;
+                 */
+
+
+            }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of an array:");
