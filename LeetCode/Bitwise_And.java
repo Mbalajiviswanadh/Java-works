24b4bad8c870da05e36a54e6961a9a94abdbc4e3
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-21 22:30:28 +0530

    :)

--- LeetCode/Bitwise_And.java
+++ LeetCode/Bitwise_And.java
@@ -1,4 +1,13 @@
 import java.util.*;
 public class Bitwise_And {
-    
+    public static int bitwise_And(int left , int right){
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the left and right values:");
+        int left = sc.nextInt();
+        int right = sc.nextInt();
+
+    }
 }
