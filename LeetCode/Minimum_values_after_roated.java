6ceb86acc1f99b89e9c3c2f6a7aa6e8a98d405a6
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-21 23:27:33 +0530

    :)

---
+++ LeetCode/Minimum_values_after_roated.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Minimum_values_after_roated {
+    
+    public static int minAfterRoated(int n, int nums[]){
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an array:");
+        int n = sc.nextInt();
+
+        int nums[] = new int[n];
+
+        System.out.println("Enter tha values inside :");
+        
+        for(int i=0;i<n;i++){
+            nums[i]=sc.nextInt();
+        }
+    }
+}
