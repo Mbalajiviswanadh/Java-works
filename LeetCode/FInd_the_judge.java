9d117533e40d59d5537bb6390566c6ce04d2b529
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-22 16:49:04 +0530

    :}

--- LeetCode/FInd_the_judge.java
+++ LeetCode/FInd_the_judge.java
@@ -3,5 +3,17 @@ public class FInd_the_judge {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Sze of the array:");
+        int n =sc.nextInt();
+
+        int trust_array[][] = new int[n][n];
+
+        System.out.println("Enter the values :");
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                trust_array[i][j] =sc.nextInt();
+            }
+        }
     }
 }
