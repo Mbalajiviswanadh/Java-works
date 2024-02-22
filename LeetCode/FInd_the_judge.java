d15dd28cc59cb5236d2373657b25642eda6c245c
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-22 17:06:16 +0530

    Finding the Town judge

--- LeetCode/FInd_the_judge.java
+++ LeetCode/FInd_the_judge.java
@@ -1,37 +1,40 @@
 import java.util.*;
+
 public class FInd_the_judge {
     
-    public static int finding_theJudge(int trust_array[][],int n){
+    public static int findTheJudge(int trust_array[][], int n) {
+        int count[] = new int[n + 1];
 
-        // int n_value=trust_array[0][0];
-
-        int count[] = new int[n+1];
-
-        for(int t[] : trust_array){
+        for (int t[] : trust_array) {
             count[t[0]]--;
             count[t[1]]++;
         }
 
-        for(int i=1;i<=n;i++){
-            if(count[i]==n-1){
+        for (int i = 1; i <= n; i++) {
+            if (count[i] == n - 1) {
                 return i;
             }
         }
         return -1;
     }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Sze of the array:");
-        int n =sc.nextInt();
+        System.out.println("Enter the number of people:");
+        int n = sc.nextInt();
 
-        int trust_array[][] = new int[n][n];
+        System.out.println("Enter the number of trust relationships:");
+        int m = sc.nextInt();
 
-        System.out.println("Enter the values :");
+        int trust_array[][] = new int[m][2];
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                trust_array[i][j] =sc.nextInt();
-            }
+        System.out.println("Enter trust relationships (one pair per line):");
+        for (int i = 0; i < m; i++) {
+            trust_array[i][0] = sc.nextInt();
+            trust_array[i][1] = sc.nextInt();
         }
+
+        int result = findTheJudge(trust_array, n);
+        System.out.println("The Town Judge is: " + result);
     }
 }
