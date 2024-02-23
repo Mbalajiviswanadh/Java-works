a5858831b5115a5048c2ec8e3d69443ed93a7f48
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-23 18:21:56 +0530

    :}

--- LeetCode/Counting_Bits.java
+++ LeetCode/Counting_Bits.java
@@ -1,5 +1,20 @@
 import java.util.*;
 public class Counting_Bits {
+
+    public static int[] ccouningBits(int n){
+
+        //creating new array 
+        int new_nums[] = new int[n+1];
+
+        for(int i=0;i<=n;i++){
+            new_nums[i] = new_nums[i/2] +i%2;
+        }
+        return new_nums;
+
+    }
+
+
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Number:");
