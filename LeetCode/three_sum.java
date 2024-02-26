f9debbda8dc40268b99520a8b2df1872dcb9263e
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-26 22:41:46 +0530

    3 sum

---
+++ LeetCode/three_sum.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class three_sum {
+
+    public static List<List<Integer>> threeSum(int nums[],int n){
+
+        if(nums== null || n<3){
+            return new ArrayList<>();
+        }
+
+        Arrays.sort(nums);
+        HashSet<List<Integer>> ans = new HashSet<>();
+
+        for(int i=0;i<n-2;i++){
+            int left=i+1;
+            int right =n-1;
+
+            while(left<right){
+                int sum = nums[i] + nums[left] +nums[right];
+            }
+        }
+
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n  = sc.nextInt();
+
+
+        int nums[] = new int[n];
+
+        for(int i=0;i<n;i++){
+            nums[i] = sc.nextInt();
+        }
+    }
+
+    
+}
