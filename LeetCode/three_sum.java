171016379a0e4993b2e689d28892226e6a3661d8
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-26 22:49:35 +0530

    :}

--- LeetCode/three_sum.java
+++ LeetCode/three_sum.java
@@ -36,14 +36,28 @@ public class three_sum {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an array :");
         int n  = sc.nextInt();
 
 
         int nums[] = new int[n];
-
+        System.out.println("Enter the values inside :");
         for(int i=0;i<n;i++){
             nums[i] = sc.nextInt();
         }
+
+
+        // calling the function 
+
+        List<List<Integer>> result = threeSum(nums, n);
+        System.out.println("Reslt: ");
+        for(List<Integer> i : result){
+            for(int j: i){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        } 
+        
     }
 
     
