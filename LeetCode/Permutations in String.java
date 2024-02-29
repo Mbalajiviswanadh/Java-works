8bb0f327b1f1e4bb67df2b9aca57ec41567df0e4
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-02-29 23:13:14 +0530

    :}

---
+++ LeetCode/Permutations in String.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Permutations_in_String {
+
+    public static boolean isPresnetInS2_orNot(String s1, String s2){
+
+        // creating an array for mapping all the alphabets chars
+        int map_array[]=new int[26];
+
+
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String value for S1 (String 1) :");
+        String s1 = sc.nextLine();
+
+        System.out.println("Enter the String value for S2 (String 2) :");
+        String s2 = sc.nextLine();
+
+        
+    }
+}
