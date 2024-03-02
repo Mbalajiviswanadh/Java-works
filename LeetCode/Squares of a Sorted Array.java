6f3bcb40c614486c9157ae104ac33feb16f0f6aa
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-03-02 17:00:08 +0530

    Squares of a Sorted Array

---
+++ LeetCode/Squares of a Sorted Array.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Squares_SortedArray {
+
+    public static int[] SquaringandSorting(int nums[], int n){
+        // creating the new array for storing the answer
+
+        int ans[]=new int[n];
+
+        for(int i=0;i<n;i++){
+            // doing squaring 
+            ans[i]=nums[i]*nums[i];
+            
+        }
+        Arrays.sort(ans);
+        return ans;
+
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an array :");
+        int n =sc.nextInt();
+
+        int nums[]=new int[n];
+        System.out.println("insert the values insde :");
+        for(int i=0;i<n;i++){
+            nums[i]=sc.nextInt();
+        }
+        // 
+    }
+}
