8d452746f37376eab186657e6368c35cf24ef788
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-03-03 20:49:02 +0530

    :}

---
+++ LeetCode/Valid Palindrome.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Valid_Palindrome {
+    public static boolean isPalindrome(String s){
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the String input:");
+        String s =sc.nextLine();
+
+
+    }    
+}
