fa6c453ae34b696083780586fc30fcdd1089381e
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-03-04 19:45:34 +0530

    add last

--- General_Practice/Add and Delete In List.java
+++ General_Practice/Add and Delete In List.java
@@ -14,7 +14,7 @@ class Add_DElete_inList{
     }
 
    }    
-
+    //  adding at the first
    public void addFirst(String data){
 
         Node newNode= new Node(data);
@@ -28,27 +28,49 @@ class Add_DElete_inList{
 
    }
 
-   public void printout(){
+//    adding at the last
+   public void addLast(String data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+            return;
+        }
 
+        Node curr_node=head;
+        while(curr_node.next!=null){
+            curr_node=curr_node.next;
+        }
+        curr_node.next=newNode;
+    }
+    // printing the list
+
+   public void printout(){
+    Node curr_node=head;
     if(head==null){
         System.out.println("List is Empty");
         return;
     }
 
-    Node curr_node=head;
     while(curr_node!=null){
         System.out.print(curr_node.data+" -> ");
         // helps in itrating
         curr_node=curr_node.next;
 
     }
-    System.out.print("NUll");
-   }
+    System.out.println("NUll");
+}
 
    public static void main(String[] args) {
     Add_DElete_inList list=new Add_DElete_inList();
 
     list.addFirst("name");
+    list.addFirst("my");
+    list.printout();
+
+    list.addLast("is");
+    list.printout();
+
+    list.addLast("vissy");
     list.printout();
     
    }
