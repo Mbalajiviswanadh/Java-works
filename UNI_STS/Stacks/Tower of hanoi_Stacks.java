7db06b1dbdaf5010ab228e920d8395aecd17f1dd
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-03-06 20:24:22 +0530

    Tower of hanoi stakcs

---
+++ UNI_STS/Stacks/Tower of hanoi_Stacks.java
@@ -0,0 +1,19 @@
+package UNI_STS.Stacks;
+import java.util.*;
+class Tower_of_hanoi_Stacks {
+    public static void movedisks(Stack<Integer> s1, Stack<Integer> s2)
+
+    public static void finding_ways(int n){
+        int total_numof_moves=(int) Math.pow(2, n)-1;
+
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter No od Disks: ");
+        int n =sc.nextInt();
+
+
+    }
+}
