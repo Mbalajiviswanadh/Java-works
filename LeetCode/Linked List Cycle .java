c753d998b2c93f75c259ba2a902298460c5a8169
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-03-06 21:48:05 +0530

    cycle or not

--- LeetCode/Linked List Cycle .java
+++ LeetCode/Linked List Cycle .java
@@ -11,6 +11,21 @@ class LinkedList_Cycle{
         }
     }
 
+    public static boolean isitCycle(ListNode head){
+
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while(slow != null && fast !=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+
+            if(slow==fast)
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ListNode head;
