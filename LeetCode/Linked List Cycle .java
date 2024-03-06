c57f30a03a0f0c7b484c4649b5611963c117ab04
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-03-06 22:53:46 +0530

    /

--- LeetCode/Linked List Cycle .java
+++ LeetCode/Linked List Cycle .java
@@ -1,43 +1,90 @@
 import java.util.*;
 
-class LinkedList_Cycle{
-    class ListNode{
+class LinkedList_Cycle {
+    static class ListNode {
         int data;
         ListNode next;
 
-        ListNode(int data){
-            this.data=data;
-            this.next=null;
+        ListNode(int data) {
+            this.data = data;
+            this.next = null;
         }
     }
 
-    public static boolean isitCycle(ListNode head){
+    // Function to insert data into a linked list
+    public static ListNode insert(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
 
-        ListNode slow=head;
-        ListNode fast=head;
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
 
-        while(slow != null && fast !=null && fast.next!=null){
-            fast=fast.next.next;
-            slow=slow.next;
+        for (int num : nums) {
+            current.next = new ListNode(num);
+            current = current.next;
+        }
+        return dummy.next;
+    }
 
-            if(slow==fast)
-            return true;
+    // Function to determine if there's a cycle in the linked list
+    public static boolean isitCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast)
+                return true;
         }
         return false;
     }
 
+    // Function to get the node at a specific position in the linked list
+    private static ListNode getNodeAtPosition(ListNode head, int pos) {
+        ListNode current = head;
+        for (int i = 0; i < pos; i++) {
+            current = current.next;
+        }
+        return current;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ListNode head;
-        ListNode tail;
+        System.out.println("Enter the size of the list:");
+        int n = sc.nextInt();
 
-        System.out.println("Enter the size of the list :");
-        int n =sc.nextInt();
+        System.out.println("Enter the values:");
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
 
-        int arr[]=new int[n];
+        System.out.println("Give the position to create the cycle (0-indexed):");
+        int pos = sc.nextInt();
 
-        System.out.println("Enter the values inside :");
+        // Create the linked list
+        ListNode head = insert(nums);
 
-       
+        // Establish the cycle at the specified position
+        ListNode cycleNode = head;
+        for (int i = 0; i < n; i++) {
+            if (i == pos) {
+                cycleNode = getNodeAtPosition(head, pos);
+                break;
+            }
+            head = head.next;
+        }
+        ListNode tail = getNodeAtPosition(head, n - 1);
+        if (tail != null) {
+            tail.next = cycleNode;
+        }
+
+        // Check if there's a cycle in the linked list
+        boolean hasCycle = isitCycle(head);
+
+        System.out.println("Result: " + hasCycle);
     }
 }
