ca939b9944bec0a2c5f180a64a68d9be7b66cd69
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-03-06 21:48:24 +0530

    {

--- UNI_STS/Stacks/Tower of hanoi_Stacks.java
+++ UNI_STS/Stacks/Tower of hanoi_Stacks.java
@@ -1,7 +1,6 @@
 package UNI_STS.Stacks;
 import java.util.*;
 class Tower_of_hanoi_Stacks {
-    public static void movedisks(Stack<Integer> s1, Stack<Integer> s2)
 
     public static void finding_ways(int n){
         int total_numof_moves=(int) Math.pow(2, n)-1;
