da456c42a7c940716a876039fbcbf784c72e938a
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-03-09 22:11:06 +0530

    Minimum Common Value

--- LeetCode/Minimum Common Value.java
+++ LeetCode/Minimum Common Value.java
@@ -7,7 +7,7 @@ class Mini_common_values{
         int j=0;
 
         while(i<n1 && j<n2){
-            if(nums1[i]==nums2[2]){
+            if(nums1[i]==nums2[j]){
                 return nums1[i];
             }
             else if(nums1[i]>nums2[j])
@@ -21,15 +21,16 @@ class Mini_common_values{
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array 1 :");
         int n1=sc.nextInt();
-        
-        System.out.println("Enter the size of the array 2 :");
-        int n2=sc.nextInt();
 
         int nums1[]=new int[n1];
         System.out.println("Enter the values inside :");
         for(int i=0;i<n1;i++){
             nums1[i]=sc.nextInt();
         }
+        
+        System.out.println("Enter the size of the array 2 :");
+        int n2=sc.nextInt();
+
         int nums2[]=new int[n2];
         System.out.println("Enter the values inside :");
         for(int i=0;i<n2;i++){
