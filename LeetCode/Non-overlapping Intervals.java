49a8b46afc788c343444a6b7b31bd297563126a7
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-03-19 21:51:55 +0530

    Non-overlapping Intervals

---
+++ LeetCode/Non-overlapping Intervals.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Non_Overlapping_intervals{
+
+    public static int findingTheInerval(int intervals[][],int n){
+        Arrays.sort(intervals,Comparator.comparingInt(a -> a[1]));
+
+        int count=1;
+        int prev_num=intervals[0][1];
+
+        for(int i=1;i<n;i++){
+            if(intervals[i][0]<prev_num)
+            continue;
+
+            count++;
+            prev_num=intervals[i][1];
+        }
+        return n-count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the rows :");
+        int n = sc.nextInt();
+
+        System.out.println("Enter the values inside :");
+        int intervals[][]=new int[n][2];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<2;j++){
+                intervals[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("INput :");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<2;j++){
+                System.out.print(intervals[i][j]+" ");
+            }
+        }
+        System.out.println();
+
+        System.out.println("Result : "+findingTheInerval(intervals, n));
+    }
+}
