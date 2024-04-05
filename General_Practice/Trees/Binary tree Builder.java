9511e1a70444becdc96ae3b365183dc8b504605c
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-04-05 22:03:25 +0530

    BT example

---
+++ General_Practice/Trees/Binary tree Builder.java
@@ -0,0 +1,69 @@
+package Trees;
+
+import java.util.*;
+
+class Binary_tree_Builder {
+    TreeNode root;
+    static class TreeNode{
+        TreeNode left,right;
+        int data;
+        TreeNode(int data){
+            this.data=data;
+        }
+    }
+    
+    public  void preOrder(TreeNode currroot){
+        if(currroot==null){
+            return;
+        }
+
+        System.out.print(currroot.data+" ");
+        preOrder(currroot.left);
+        preOrder(currroot.right);   
+    }
+    public TreeNode buildTree(String strTree[], Binary_tree_Builder bt){
+        
+
+        if(strTree[0].equals("N"))
+        return null;
+
+        bt.root=new TreeNode(Integer.parseInt(strTree[0]));
+        Queue<TreeNode> q= new LinkedList<TreeNode>();
+        q.add(bt.root);
+        int i=1;
+        while(i<strTree.length){
+            TreeNode curr=q.poll();
+            if(!strTree[i].equals("N")){
+                curr.left=new TreeNode(Integer.parseInt(strTree[i]));
+                q.add(curr.left);
+            }
+            i++;
+            if(i>=strTree.length){
+                break;
+            }
+
+            if(!strTree[i].equals("N")){
+                curr.right = new TreeNode(Integer.parseInt(strTree[i]));
+                q.add(curr.right);
+            }
+            i++;
+        }
+        return bt.root;
+
+    }
+
+
+    public static void main(String[] args) {
+        Binary_tree_Builder bt=new Binary_tree_Builder();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Build the Tree : ");
+        String[] strTree=sc.nextLine().split(" ");
+        bt.buildTree(strTree,bt);
+
+
+        System.out.println("\nPre Order -B_Tree:");
+        bt.preOrder(bt.root);
+
+
+    }
+}
--- UNI_STS/Linked_List/Seggregate.java
+++ UNI_STS/Linked_List/Seggregate.java
@@ -1,5 +1,85 @@
 package UNI_STS.Linked_List;
 
-public class Seggregate {
+import java.util.Scanner;
+class Main {
+    Node head;
+    static class Node {
+        int data;
+        Node next;
+        public Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+    public static Main insert(Main list, int data) {
+        Node new_node = new Node(data);
+        if (list.head == null) {
+            list.head = new_node;
+        } else {
+            Node last = list.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = new_node;
+        }
+        return list;
+    }
+    public static void printList(Main list) {
+        Node currNode = list.head;
+        while (currNode != null) {
+            System.out.print(currNode.data + " ");
+            currNode = currNode.next;
+        }
+    }
+    public static Main segregateNodes(Main list) {
+        if (list.head == null || list.head.next == null) {
+            return list;
+        }
+        Main oddList = new Main();
+        Main evenList = new Main();
+        Node curr = list.head;
+        Node oddTail = null;
+        Node evenTail = null;
+        boolean isOdd = true;
+        while (curr != null) {
+            if (isOdd) {
+                oddTail = insertNode(oddList, curr.data, oddTail);
+            } else {
+                evenTail = insertNode(evenList, curr.data, evenTail);
+            }
+            isOdd = !isOdd;
+            curr = curr.next;
+        }
+        if (oddTail != null) {
+            oddTail.next = evenList.head;
+        } else {
+            return evenList;
+        }
+        return oddList;
+    }
+    public static Node insertNode(Main list, int data, Node tail) {
+        Node new_node = new Node(data);
+        if (list.head == null) {
+            list.head = new_node;
+            tail = new_node;
+        } else {
+            tail.next = new_node;
+            tail = new_node;
+        }
+        return tail;
+    }
+    public static void main(String[] args) {
+        Main ll = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String str = sc.nextLine();
+        String arr[] = str.split(" ");
 
+        for (int i = 0; i < n; i++) {
+            insert(ll, Integer.parseInt(arr[i]));
+        }
+        Main result = segregateNodes(ll);
+        printList(result);
+    }
 }
