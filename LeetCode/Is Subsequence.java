32ae555687156e952af4ab533d1862dad2004a17
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-04-10 17:58:53 +0530

    Is Subsequence problem

---
+++ LeetCode/Is Subsequence.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+
+class Is_Subsequence{
+
+    public static boolean compare(String s, String t){
+        int sPoint=0;
+        int tPoint=0;
+
+        while(sPoint<s.length() && tPoint<t.length()){
+            if(s.charAt(sPoint)==t.charAt(tPoint))
+            sPoint++;
+
+            tPoint++;
+        }
+        return sPoint==s.length();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENter the 1st String :");
+        String s=sc.nextLine();
+
+        System.out.println("Enter the 2nd String :");
+        String t=sc.nextLine();
+
+        System.out.println("Result : "+compare(s, t) );
+
+    }
+}
