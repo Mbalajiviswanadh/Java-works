795a78229d4ce7318e184e10b89b020a56c639ce
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-04-13 22:02:34 +0530

    Biary Tree Inorder

---
+++ LeetCode/Binary Tree Inorder.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+
+class Solution {
+    static class TreeNode{
+        int val;
+        TreeNode left,right;
+
+        TreeNode(int val){
+            this.val=val;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    List<Integer> l=new ArrayList<>();
+public List<Integer> inorderTraversal(TreeNode root) {
+    if(root==null)
+    return new ArrayList<>();
+
+    inorderTraversal(root.left);
+    l.add(root.val);
+    inorderTraversal(root.right);
+
+    return l;
+}
+}
