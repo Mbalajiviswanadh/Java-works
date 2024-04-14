8dda44a56367ce4e2fdfebe100cf18bc21ee1ad5
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-04-14 22:41:26 +0530

    Sum of Left Leaves

---
+++ LeetCode/Sum of Left Leaves.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Sum_ofLeftLeaves {
+    
+    static class TreeNode{
+        int val;
+        TreeNode left,right;
+
+        TreeNode(int val){
+            this.left=null;
+            this.right=null;
+            this.val=val;
+        }
+    }
+
+
+
+    class Solution {
+        public int sumOfLeftLeaves(TreeNode root) {
+    
+            int res=0;
+    
+            if(root==null)        
+            return res;
+    
+            if(root.left!=null){
+                if(root.left.left==null && root.left.right==null)
+                res+=root.left.val;
+                else
+                res+=sumOfLeftLeaves(root.left);
+            }
+            res+=sumOfLeftLeaves(root.right);
+    
+            return res;
+        }
+    }
+}
