c1d88009db0c6a8d6bd6a38d668771b58b4a4882
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-08-24 16:43:13 +0530

    single number 1

---
+++ LeetCode/Single number 2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        Arrays.sort(nums);
+
+
+        for(int i=1;i<nums.length;i=i+3){
+            int j=i-1;
+            if(nums[i]!=nums[j]){
+                return nums[j];
+            }
+        }
+        return nums[nums.length-1];
+
+
+        // Map<Integer, Integer> map = new HashMap<>();
+
+        // for(int i: nums){
+        //     map.put(i, map.getOrDefault(i,0)+1);
+        // }
+
+        // for(int i: nums){
+        //     if(map.get(i)==1)
+        //     return i;
+        // }
+        // return 0;
+    }
+}
