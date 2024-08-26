941c8439838843a09a5915c17a1fd4cc6594bfe2
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-08-26 09:48:14 +0530

    Rearrange a LL in zigzag

---
+++ Arsh-DSA-Sheet/Rearrange a LL.java
@@ -0,0 +1,53 @@
+
+import java.util.*;
+
+class Solution {
+    Node zigZag(Node head) {
+        // add code here.
+        
+        if(head==null || head.next==null)
+        return head;
+        
+        Node res =new Node (0);
+        Node temp=res;
+        
+        boolean flag=true;
+        
+        Node curr=head;
+        
+        while(curr!=null && curr.next!=null){
+            
+            if(flag){
+                
+                if(curr.data < curr.next.data){
+                    temp.next=curr;
+                    curr=curr.next;
+                }
+                else{
+                    temp.next=curr.next;
+                    curr.next=curr.next.next;
+                }
+            }
+            else{
+                if(curr.data > curr.next.data){
+                    temp.next=curr;
+                    curr=curr.next;
+                }
+                else{
+                    temp.next=curr.next;
+                    curr.next=curr.next.next;
+                }
+            }
+            
+            temp=temp.next;
+            flag =!flag;
+
+        }
+        
+        if(curr!=null)
+        temp.next=curr;
+        
+        return res.next;
+
+    }
+}
