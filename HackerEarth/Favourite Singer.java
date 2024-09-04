6a5a78e04a6a13bf069b2197fac884624fbf2582
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-09-04 12:11:24 +0530

    hackerearth

---
+++ HackerEarth/Favourite Singer.java
@@ -0,0 +1,31 @@
+package HackerEarth;
+
+import java.util.*;
+
+class TestClass{
+
+    public static void main(String[] args){
+        Scanner sc =  new Scanner(System.in);
+
+        int n=sc.nextInt();
+
+        Map<Long, Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++){
+            long singer= sc.nextLong();
+
+            map.put(singer, map.getOrDefault(singer,0)+1);
+        }
+
+        int maxnum=0;
+        for(int i: map.values()){
+            maxnum=Math.max(maxnum,i);
+        }
+
+        int count=0;
+        for(int i:map.values()){
+            if(maxnum==i)
+            count++;
+        }
+        System.out.print(count);
+    }
+}
