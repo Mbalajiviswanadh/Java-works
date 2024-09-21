82fa9a743ad0746bf49a48c9cd226328f5c48b46
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-09-21 11:25:38 +0530

    simplify path

---
+++ LeetCode/Simplify Path.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+
+        String str[]= path.split("/");
+
+        for(int i=0;i<str.length;i++){
+
+            if( str[i].equals("") ||str[i].equals("."))
+            continue;
+
+            if(str[i].equals("..")){
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+            }
+            else
+            stack.push(str[i]);
+        }
+
+        StringBuilder res = new StringBuilder();
+
+        for(String i: stack){
+            res.append("/").append(i);
+        }
+
+        if(res.length()>0)
+        return res.toString();
+        else 
+        return "/";
+        
+    }
+} Simplify Path {
+    
+}
