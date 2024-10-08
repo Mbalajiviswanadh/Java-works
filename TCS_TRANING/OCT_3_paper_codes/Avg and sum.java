49b2446ef86d6e150d6534e021a713b7f41b15c4
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-10-08 15:45:07 +0530

    use of printf and %.1f

--- TCS_TRANING/OCT_3_paper_codes/Avg and sum.java
+++ TCS_TRANING/OCT_3_paper_codes/Avg and sum.java
@@ -21,7 +21,10 @@ class Avg_and_Sum{
         }
         double avg=(double)sum/nums.length;
         
-        System.out.print("sum: "+sum+"\n"+"Avg: "+avg);
+        System.out.printf("sum: "+sum+"\n"+"Avg: %.1f",avg);
+        System.out.println();
         
+        System.out.println("Sum: "+sum);
+        System.out.printf("Avg: %.1f",avg);
     }
 }
