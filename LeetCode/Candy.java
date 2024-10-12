7013baf9abcd650a756abd1f007ade0f310a39a7
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-10-12 10:11:19 +0530

    candy

---
+++ LeetCode/Candy.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Candy{
+
+    public static int numOfCandies(int rating[]){
+
+         // L to R -> r[i]> r[i-1] => c[i]=c[i-1]+1
+        // R to L -> r[i]> r[i+1] => c[i]=max(c[i],c[i+1]+1);
+
+        int candies[]= new int[rating.length];
+
+        candies[0]=1;
+
+        for(int i=1;i<rating.length;i++){
+
+            if(rating[i]> rating[i-1])
+            candies[i]=candies[i-1]+1;
+            else
+            candies[i]=1;
+        }
+
+        for(int i=rating.length-2;i>=0;i--){
+
+            if(rating[i]> rating[i+1])
+            candies[i]=Math.max(candies[i], candies[i+1]+1);
+        }
+
+        int sum=0;
+
+        for(int i: candies){
+            sum+=i;
+        }
+        return sum;
+    }
+}
