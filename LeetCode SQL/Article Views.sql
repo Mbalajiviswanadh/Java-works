ae15b27b3b2a2f0c7e76e8c41365c929ce77baeb
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-11-03 19:02:10 +0530

    sql select

---
+++ LeetCode SQL/Article Views.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT author_id as id FROM Views WHERE author_id=viewer_id ORDER BY author_id ASC
+
+-- USED
+-- DISTINCT for single valus
+-- ORDER BY __ ASC for accedning order
---
+++ LeetCode SQL/Find Customer Referee.sql
@@ -0,0 +1,5 @@
+SELECT name FROM Customer WHERE NOT referee_id=2 OR referee_id IS null
+
+
+--
+-- select name from customer where referee_id!=2 or regeree_id is null
