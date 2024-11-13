2c4ed29be4bffd59533c4983b1106c206580978b
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-11-13 22:35:36 +0530

    count the number of pairs

---
+++ LeetCode/Count the number of Pairs.java
@@ -0,0 +1,38 @@
+class Solution {
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+
+        long res=0;
+
+        for(int i=0;i<nums.length;i++){
+            int low= lowerBound(nums, i+1, nums.length, lower-nums[i]);
+            int up = upperBound(nums, i+1, nums.length, upper-nums[i]);
+            res+= up-low;
+        }
+        return res;
+    }
+
+    public int lowerBound(int nums[], int l, int r, int target){
+        while(l<r){
+            int mid = l+(r-l)/2;
+
+            if(nums[mid]>=target)
+            r =mid;
+            else
+            l=mid+1;
+        }
+        return l;
+    }
+    public int upperBound(int nums[], int l, int r, int target){
+        while(l<r){
+            int mid = l+(r-l)/2;
+
+            if(nums[mid]>target)
+            r =mid;
+            else
+            l=mid+1;
+        }
+        return l;
+    }
+
+}
