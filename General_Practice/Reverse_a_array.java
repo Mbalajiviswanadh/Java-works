dfaad551871a0d8cc90fcc06ab34db4bd1b7de0c
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-11-29 13:43:15 +0530

    -LL

---
+++ General_Practice/Reverse_a_array.java
@@ -0,0 +1,17 @@
+
+
+class Solution {
+    public void reverseArray(int arr[]) {
+        // code here
+        
+        
+        int n = arr.length;
+        for(int i=0;i<arr.length/2;i++){
+            
+            int temp = arr[n-i-1];
+            arr[n-i-1]= arr[i];
+            arr[i]=temp;
+        }
+        
+    }
+}
